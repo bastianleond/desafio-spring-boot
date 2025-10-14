ccaacd5600a14116100eaa8d1597bb954bdeabfc
Bastián León <bastianleond@gmail.com>
2025-10-13 22:31:07 -0300

    ajustes de code review analytics.

--- src/main/java/com/nuevospa/taskmanager/config/SecurityConfig.java
+++ src/main/java/com/nuevospa/taskmanager/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.nuevospa.taskmanager.config;
 
-import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
